eed9ec19f2b54264fb6fc6c8f52b915837691e5e
Artem Zhitkov <aptem_deadly@mail.ru>
2025-01-18 21:33:40 +0500

    create project

---
+++ mailapp/models.py
@@ -0,0 +1,65 @@
+from django.db import models
+
+
+class RecipientMail(models.Model):
+    email = models.EmailField(max_length=100, unique=True, verbose_name='Email')
+    full_name = models.CharField(max_length=100, verbose_name='Ф.И.О.')
+    comment = models.TextField(verbose_name='Комментарий', blank=True, null=True)
+
+    def __str__(self):
+        return self.full_name
+
+    class Meta:
+        verbose_name = 'Получатель письма'
+        verbose_name_plural = 'Получатели писем'
+        ordering = ['full_name']
+
+
+class MailMessage(models.Model):
+    subject = models.CharField(max_length=200, verbose_name='Тема письма')
+    body = models.TextField(verbose_name='Тело письма')
+
+    def __str__(self):
+        return self.subject
+
+    class Meta:
+        verbose_name = 'Сообщение'
+        verbose_name_plural = 'Сообщения'
+        ordering = ['subject']
+
+
+class Mailing(models.Model):
+    CREATED = 'Создана'
+    STARTED = 'Запущена'
+    FINISHED = 'Завершена'
+    MINUTLY = 'minutly'
+    DAILY = 'daily',
+    WEEKLY = 'weekly',
+    MONTHLY = 'monthly',
+
+    STATUS_CHOICES = (
+        (CREATED, 'Создана'),
+        (STARTED, 'Запущена'),
+        (FINISHED, 'Завершена'),
+    )
+
+    FREQUENCY_CHOICES = (
+        ('MINUTLY', 'раз в минуту'),
+        ('DAILY', 'раз в день'),
+        ('WEEKLY', 'раз в неделю'),
+        ('MONTHLY', 'раз в месяц'),
+    )
+
+    start_time = models.DateTimeField(verbose_name='Дата и время первой отправки', blank=True, null=True)
+    end_time = models.DateTimeField(verbose_name='Дата и время последней отправки', blank=True, null=True)
+    status = models.CharField(verbose_name='Статус рассылки', default=CREATED, choices=STATUS_CHOICES)
+    message = models.ForeignKey(MailMessage, verbose_name='Сообщение', on_delete=models.CASCADE, blank=True, null=True)
+    recipients = models.ManyToManyField(RecipientMail, verbose_name='Получатели писем')
+
+    def __str__(self):
+        return f'{self.message} - {self.status}'
+
+    class Meta:
+        verbose_name = 'Рассылка'
+        verbose_name_plural = 'Рассылки'
+        ordering = ['-start_time']

40b73486fa021413556b9f69115c77d3d6337af3
Artem Zhitkov <aptem_deadly@mail.ru>
2025-02-10 22:03:16 +0500

    добавлены контроллеры рассылки и сообщений

--- mailapp/views.py
+++ mailapp/views.py
@@ -11,8 +11,6 @@ class RecipientMailListViews(ListView):
     ordering = ['full_name']
 
 
-
-
 class RecipientMailDetailViews(DetailView):
     model = RecipientMail
     template_name = 'mailapp/recipient_mail_detail.html'
@@ -44,3 +42,57 @@ class RecipientMailDeleteViews(DeleteView):
     template_name = 'recipient_mail_delete.html'
     success_url = reverse_lazy('mailapp:recipient_mail_list')
 
+
+class MailMessageListView(ListView):
+    model = MailMessage
+    template_name = 'mailapp/mail_message_list.html'
+    context_object_name = 'messages'
+    paginate_by = 10
+
+class MailMessageCreateView(CreateView):
+    model = MailMessage
+    template_name = 'mailapp/mail_message_form.html'
+    fields = ['subject', 'body']
+    success_url = reverse_lazy('mailapp:mail_message_list')
+
+class MailMessageUpdateView(UpdateView):
+    model = MailMessage
+    template_name = 'mailapp/mail_message_form.html'
+    fields = ['subject', 'body']
+    success_url = reverse_lazy('mailapp:mail_message_detail')
+
+    def get_success_url(self):
+        message = self.object
+        return reverse_lazy('mailapp:mail_message_detail', kwargs={'pk': message.pk})
+
+class MailMessageDetailView(DetailView):
+    model = MailMessage
+    template_name = 'mailapp/mail_message_detail.html'
+    context_object_name = 'message'
+
+class MailMessageDeleteView(DeleteView):
+    model = MailMessage
+    template_name = 'mail_message_confirm_delete.html'
+    success_url = reverse_lazy('mailapp:mail_message_list')
+
+
+class MailingCreateView(CreateView):
+    model = Mailing
+    template_name = 'mailapp/mailing_form.html'
+    fields = ['message', 'recipients']
+    success_url = reverse_lazy('mailapp:mailing_list')
+
+
+
+class MailingDetailView(DetailView):
+    model = Mailing
+    template_name = 'mailapp/mailing_detail.html'
+    context_object_name = 'mailing'
+
+
+
+class MailingListView(ListView):
+    model = Mailing
+    template_name = 'mailapp/mailing_list.html'
+    context_object_name = 'mailings'
+    paginate_by = 10

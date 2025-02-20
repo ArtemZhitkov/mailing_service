0a37e64804fad53961e9621f99b16307d79e4975
Artem Zhitkov <aptem_deadly@mail.ru>
2025-02-20 20:17:34 +0500

    добавлено поле verification_token в модель User

--- users/models.py
+++ users/models.py
@@ -9,6 +9,7 @@ class User(AbstractUser):
     avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
     phone_number = models.CharField(max_length=20, blank=True, null=True)
     area = models.CharField(max_length=50, blank=True, null=True)
+    verification_token = models.CharField(max_length=100, verbose_name='Token', blank=True, null=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []

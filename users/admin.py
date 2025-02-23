fbbe60a6c98aebf8223e81e1d2bbc2a5a7da6547
Artem Zhitkov <aptem_deadly@mail.ru>
2025-02-23 22:39:19 +0500

    add UserAdmin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'avatar', 'phone_number', 'area')
+    search_fields = ('email', 'area')

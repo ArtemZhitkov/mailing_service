4f262f48f17c20e053a0eb7ad5c679613dd17bf6
Artem Zhitkov <aptem_deadly@mail.ru>
2025-02-23 22:41:37 +0500

    add UserListView

--- users/views.py
+++ users/views.py
@@ -1,41 +1,37 @@
 from django.urls import reverse_lazy, reverse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views.generic.edit import CreateView, UpdateView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic import DetailView
 from .models import User
 from .forms import UserRegisterForm, PasswordResetForm
 from .services import UserService
 
+class UsersListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
+    model = User
+    template_name = 'users/user_list.html'
+    permission_required = 'users.can_view_user_list'
+    context_object_name = 'users'
+
 
 class UserCreateView(CreateView):
     template_name ='users/register.html'
     form_class = UserRegisterForm
-    success_url = reverse_lazy('mailapp:mailing_list')
+    success_url = reverse_lazy('users:login')
 
     def form_valid(self, form):
         user = form.save()
         user.is_active = False
-        UserService.send_verification_email(user)
+        host = self.request.get_host()
+        UserService.send_verification_email(user, host)
         return super().form_valid(form)
 
-def email_verification(request, token):
-    user = get_object_or_404(User, token=token)
+def email_verification(request, verification_token):
+    user = get_object_or_404(User, verification_token=verification_token)
     user.is_active = True
     user.save()
     return redirect(reverse('users:login'))
-
-def reset_password(request, token):
-    user = get_object_or_404(User, token=token)
-    if request.method == 'POST':
-        form = PasswordResetForm(request.POST)
-        if form.is_valid():
-            user.set_password(form.cleaned_data['password'])
-            user.save()
-            return redirect(reverse('users:login'))
-    else:
-        form = PasswordResetForm()
-    return render(request, 'users/reset_password.html', {'form': form})
 
 
 class UserProfileUpdateView(UpdateView):

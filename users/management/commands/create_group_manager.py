632fbaa6de4429e783ef72dac8d9eeecc093bfd7
Artem Zhitkov <aptem_deadly@mail.ru>
2025-02-23 22:36:57 +0500

    кастомная команда создания группы 'Manager'

---
+++ users/management/commands/create_group_manager.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import Permission, Group
+
+class Command(BaseCommand):
+    help = 'Create permissions and groups'
+
+    def handle(self, *args, **options):
+        manager_group = Group.objects.create(name='Manager')
+
+        view_user_list = Permission.objects.get(codename='can_view_user_list')
+        blocking_user = Permission.objects.get(codename='can_blocking_user')
+        view_recipients = Permission.objects.get(codename='can_view_recipient_mail')
+        view_mailings = Permission.objects.get(codename='can_view_mailing')
+        disabling_mailing = Permission.objects.get(codename='can_disabling_mailing')
+
+        manager_group.permissions.add(view_user_list, blocking_user, view_recipients, view_mailings, disabling_mailing)
+        manager_group.save()
+        self.stdout.write(self.style.SUCCESS('Permissions and groups created successfully'))
